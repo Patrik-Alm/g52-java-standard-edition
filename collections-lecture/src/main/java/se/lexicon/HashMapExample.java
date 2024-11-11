6dd2d28ac5f61a160cdc643a4d498945eb6b1047
Simon Elbrink <45754982+SimonElbrink@users.noreply.github.com>
2024-11-11 13:33:02 +0100

    After Lecture Map Implementation

--- collections-lecture/src/main/java/se/lexicon/HashMapExample.java
+++ collections-lecture/src/main/java/se/lexicon/HashMapExample.java
@@ -1,6 +1,9 @@
 package se.lexicon;
 
+import se.lexicon.model.Person;
+
 import java.util.HashMap;
+import java.util.Map;
 
 public class HashMapExample {
 
@@ -21,6 +24,24 @@ public class HashMapExample {
 
         System.out.println(contacts.containsKey("Mehrdad"));
         System.out.println(contacts.containsValue("Mehrdad@lexicon.se"));
+
+
+        // After Lecture
+
+        for (Map.Entry<String, String> entry: contacts.entrySet() ){
+            System.out.println("Key: " + entry.getKey() + " Value: " + entry.getValue());
+        }
+
+        HashMap<String, Person> people = new HashMap<>();
+        Person person1 = new Person(3, "John", "Doe", "jon@test.se");
+        Person person2 = new Person(1, "Alice", "Smith", "alice@test.se");
+        Person person3 = new Person(2, "Bob", "Johanson", "bob@test.se");
+
+        people.put(person1.getEmail(), person1);
+        people.put(person2.getEmail(), person2);
+        people.put(person3.getEmail(), person3);
+
+        System.out.println(people.get("bob@test.se"));
 
     }
 }
