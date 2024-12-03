503c5bb2ee3d03c809fe668ce0a437d044f7b3ec
Simon Elbrink <45754982+SimonElbrink@users.noreply.github.com>
2024-12-03 10:15:37 +0100

    Exceptions & Files Lecture - Mini Example

--- exceptions-files-lecture/src/main/java/se/lexicon/ExceptionExamples.java
+++ exceptions-files-lecture/src/main/java/se/lexicon/ExceptionExamples.java
@@ -1,9 +1,23 @@
 package se.lexicon;
 
+import java.io.BufferedReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class ExceptionExamples {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+        // Unchecked (Runtime) Exception
+//        int[] number = {1,2,3,4,5};
+//        System.out.println(number[10]);
+
+
+        // Checked (Compile Time) Exception
+//        Path filePath = Paths.get("folder/TestHere.txt");
+//        BufferedReader reader = Files.newBufferedReader(filePath);
+
+
     }
 
 }
