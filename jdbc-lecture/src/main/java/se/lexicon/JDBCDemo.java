86312d16884eee8e046afed4d06b86f4032e6cf5
Simon Elbrink <45754982+SimonElbrink@users.noreply.github.com>
2024-12-17 12:05:02 +0100

    JDBC Lecture - Connection Class + Credentials from a file.

--- jdbc-lecture/src/main/java/se/lexicon/JDBCDemo.java
+++ jdbc-lecture/src/main/java/se/lexicon/JDBCDemo.java
@@ -2,6 +2,8 @@ package se.lexicon;
 
 import java.sql.*;
 
+import static se.lexicon.db.MySQLConnection.getConnection;
+
 public class JDBCDemo {
     public static void main(String[] args) {
 
@@ -12,7 +14,8 @@ public class JDBCDemo {
         ResultSet resultSet = null;
 
         try{
-            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/01_lecture_db", "root", "1234");
+//            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/01_lecture_db", "root", "1234");
+            connection = getConnection();
             statement = connection.createStatement();
             resultSet = statement.executeQuery("SELECT * FROM task");
 
---
+++ jdbc-lecture/src/main/java/se/lexicon/db/MySQLConnection.java
@@ -0,0 +1,45 @@
+package se.lexicon.db;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class MySQLConnection {
+
+    static Properties properties = new Properties();
+
+    private static String url;
+    private static String password;
+    private static String username;
+
+
+    static {
+        try (   // In your project, the path most likely needs to be "database.properties"
+                Reader in = Files.newBufferedReader(Path.of("jdbc-lecture/src/main/resources/database.properties"), StandardCharsets.UTF_8);
+        ) {
+            properties.load(in);
+            System.out.println("File Read...");
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        System.out.println("Reading properties...");
+        url = properties.getProperty("jdbc.url");
+        username = properties.getProperty("jdbc.username");
+        password = properties.getProperty("jdbc.password");
+    }
+
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(url, username, password);
+    }
+
+
+}
